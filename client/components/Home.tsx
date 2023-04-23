5e17a46661e726a65e5357e01a17ffca6041659c
Shadae <shadaejadeellmers@gmail.com>
2023-04-23 22:55:45 +1200

    links created for pages

--- client/components/Home.tsx
+++ client/components/Home.tsx
@@ -1,14 +1,21 @@
+import { Link } from 'react-router-dom'
+
 function Home() {
   return (
     <>
       <h2>Hello 👋</h2>
       <p>
-        I&apos;m Shadae, an emerging software developer based in Pōneke,
-        Aoteroa.
+        I&apos;m Shadae, a junior software developer based in Pōneke, Aoteroa.
       </p>
-      <p>Projects →</p>
-      <p>About →</p>
-      <p>Contact →</p>
+      <div>
+        <Link to="/projects">Projects →</Link>
+      </div>
+      <div>
+        <Link to="/about">About →</Link>
+      </div>
+      <div>
+        <Link to="/contact">Contact →</Link>
+      </div>
     </>
   )
 }
--- client/components/Nav.tsx
+++ client/components/Nav.tsx
@@ -1,7 +1,12 @@
+import { Link } from 'react-router-dom'
+
 function Nav() {
   return (
     <>
-      <h2>nav</h2>
+      <Link to="/">Home</Link>
+      <Link to="/projects">Projects</Link>
+      <Link to="/about">About</Link>
+      <Link to="/contact">Contact</Link>
     </>
   )
 }
